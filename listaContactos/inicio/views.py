84e7a320fb185cb3af99053bd01007cbec7247f4
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-12 10:27:04 -0500

    Agregando la variable 'myDate' que es un objeto de tipo 'datatime' represetando la fecha y hora: 01/01/2023 a las 12:00. Ademas utilizar el filtro 'date' para formatear la fecha segun el fomrato 'D d M y'

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def myHomeView(request, *args, **kwargs):
@@ -7,6 +8,7 @@ def myHomeView(request, *args, **kwargs):
         'myText': 'Esto es sobre nosotros',
         'myNumber': 123,
         'myList': [33, 44, 55],
+        'myDate': datetime(2023, 1, 1, 12, 0),
         }
     return render(request, "home.html" , myContext)
 
