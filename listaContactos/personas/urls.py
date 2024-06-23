37b4b0c2e89c09d8fcd63fd389b74adfa963d298
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-23 18:45:57 -0500

    Nueva ruta para mostrar una lista de personas utilizando una vista basada en clase PersonaListView que utiliza la clase generica ListView de Django para manejar la lista de objetos del modelo Persona

--- listaContactos/personas/urls.py
+++ listaContactos/personas/urls.py
@@ -8,13 +8,17 @@ from personas.views import (
     personasListView,
     personasDeleteView,
     )
+from .views import (
+    PersonaListView,
+    )
 
 urlpatterns = [
+    path('', PersonaListView.as_view(), name = 'persona-list'),
     path('people/', personaTestView, name='personas'),
     path('add/', personaCreateView1, name='adding'),
     path('anotherAdd/', personasAnotherCreateView, name='anotherAdding'),
     path('search/', searchForHelp, name='buscar'),
     path('<int:myID>/', personasShowObject, name = 'browsing'),
     path('<int:myID>/delete/', personasDeleteView, name = 'deleting'),
-    path('', personasListView, name = 'listing'),
+    #path('', personasListView, name = 'listing'),
 ]
