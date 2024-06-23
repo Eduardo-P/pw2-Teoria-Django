13ec68090b93b5e6e2ae131a5811c12de1c58ae7
Eduardo <eportugalpor@unsa.edu.pe>
2024-06-23 15:29:29 -0500

    Se modifica la vista personaCreateView1 para editar la instancia de Persona con ID 2 si se proporcionan datos POST válidos, o para mostrar un formulario vacío de PersonaForm en caso contrario.

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -11,14 +11,14 @@ def personaTestView(request):
     return render(request, 'personas/descripcion.html', context)
 
 def personaCreateView1(request):
-    form = PersonaForm(request.POST or None)
+    obj = Persona.objects.get(id = 2)
+    form = PersonaForm(request.POST or None, instance = obj)
     if form.is_valid():
         form.save()
         form = PersonaForm()
-        
     context = {
         'form': form
-    }
+        }
     return render(request, 'personas/personasCreate.html', context)
 
 def personaCreateView2(request):
